21b5f2c7cf1b50d604db8d66ba639e594c497011
batisuoc <zhenghengyue@gmail.com>
2019-11-26 22:59:34 +0700

    Update product management view

--- www/admin/views/product/index.php
+++ www/admin/views/product/index.php
@@ -1,9 +1,40 @@
  <div class="container-fluid">
      <!-- Breadcrumbs-->
      <ol class="breadcrumb">
-         <div class="row">
-             <h3 class="float-left">PRODUCT PAGE</h3>
-             <button type="button" class="btn btn-primary float-right">Add product</button>
+         <div class="container">
+             <h3 class="float-left">QUẢN LÝ SẢN PHẨM</h3>
+             <button type="button" class="btn btn-primary float-right">Thêm sản phẩm</button>
          </div>
+         <!-- ./ container -->
      </ol>
+     <table class="table">
+         <thead>
+             <tr>
+                 <th scope="col">#</th>
+                 <th scope="col">First</th>
+                 <th scope="col">Last</th>
+                 <th scope="col">Handle</th>
+             </tr>
+         </thead>
+         <tbody>
+             <tr>
+                 <th scope="row">1</th>
+                 <td>Mark</td>
+                 <td>Otto</td>
+                 <td>@mdo</td>
+             </tr>
+             <tr>
+                 <th scope="row">2</th>
+                 <td>Jacob</td>
+                 <td>Thornton</td>
+                 <td>@fat</td>
+             </tr>
+             <tr>
+                 <th scope="row">3</th>
+                 <td>Larry</td>
+                 <td>the Bird</td>
+                 <td>@twitter</td>
+             </tr>
+         </tbody>
+     </table>
  </div>
